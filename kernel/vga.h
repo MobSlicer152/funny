0009d60c7a385e899ea978bb612ccdaaf7a6f450
MobSlicer152 <brambleclaw1414@gmail.com>
2024-10-29 13:07:41 -0400

    Remove MSVC header

--- kernel/vga.h
+++ kernel/vga.h
@@ -4,5 +4,12 @@
 
 typedef struct VgaMode
 {
+    u8 modeControl;
+    u8 overscan;
+    u8 colorPlaneEnable;
+    u8 horizontalPanning;
+    u8 colorSelect;
+    u8 misc;
+    u8 clockMode;
     
 } VgaMode_t;
