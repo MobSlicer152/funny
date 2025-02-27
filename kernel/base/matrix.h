b6a36bb6b9304db614e3436cb680ee2735a4823c
MobSlicer152 <brambleclaw1414@gmail.com>
2025-02-27 15:15:47 -0500

    Add M4I

--- kernel/base/matrix.h
+++ kernel/base/matrix.h
@@ -5,7 +5,15 @@
 
 typedef f32 Mat4_t[4][4];
 
-static FORCEINLINE Mat4Mul(const Mat4_t* a, const Mat4_t* b)
-{
+#define M4I(m00, m01, m02, m03, m10, m11, m12, m13, m20, m21, m22, m23, m30, m31, m32, m33)                                      \
+	(Mat4_t)                                                                                                                     \
+	{                                                                                                                            \
+		(f32[]){(m00), (m01), (m02), (m03)},                                                                                     \
+		(f32[]){(m10), (m11), (m12), (m13)},                                                                                     \
+		(f32[]){(m20), (m21), (m22), (m23)},                                                                                     \
+		(f32[]){(m30), (m31), (m32), (m33)},                                                                                     \
+	}
 
+static FORCEINLINE void Mat4Mul(const Mat4_t* a, const Mat4_t* b)
+{
 }
